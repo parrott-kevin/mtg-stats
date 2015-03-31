143637494d9eca0d2e142d9d01bde48646fca7c5
Kevin Parrott <parrott.kevin@gmail.com>
2015-03-31 13:20:20 -0400

    Configured concat and minify for build step. Part of move to typescript. This will help tie up loose ends of build.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public/dist')));
 
 app.use('/api', routes);
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 /**
  * Created by parrott-kevin on 1/6/15.
  */
+
 'use strict';
 
 var gulp = require('gulp');
 var del = require('del');
 var less = require('gulp-less');
+
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+//var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 
 //---------------------------------------------------------------------------//
 // Compilation Tasks
@@ -18,6 +24,15 @@ gulp.task('dev', [
     'fonts:dev'
   ], function() {}
 );
+
+gulp.task('dist', [
+  'clean:dist',
+  'compress:js',
+  'compress:lib',
+  'compress:css',
+  'copy:html',
+  'copy:fonts'
+]);
 
 //---------------------------------------------------------------------------//
 // Common Tasks
@@ -42,8 +57,56 @@ gulp.task('clean:dev', function() {
 // Copy fonts for dev
 gulp.task('fonts:dev', function() {
   gulp.src([
-    'public/lib/bootstrap/fonts/*',
-    'public/lib/fontawesome/fonts/*'
+    'public/lib/bootstrap/fonts/*'
   ])
     .pipe(gulp.dest('public/assets/fonts'));
+});
+
+// Clean task for dist
+gulp.task('clean:dist', function() {
+  del.sync(['public/dist/*']);
+});
+
+// Compress javascript
+gulp.task('compress:js', function() {
+  return gulp.src('./public/src/app/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.concat.js'))
+    //.pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/dist/js'));
+});
+
+gulp.task('compress:lib', function() {
+  var libDirectory = './public/src/lib/';
+  var libSrc = [
+    'angular/angular.js',
+    'angular-route/angular-route.js',
+    'angular-bootstrap/ui-bootstrap.js',
+    'angular-bootstrap/ui-bootstrap-tpls.js',
+    'lodash/lodash.js'
+  ];
+  var lib = libSrc.map(function(i) {
+    return libDirectory + i;
+  });
+
+  return gulp.src(lib)
+    .pipe(concat('lib.concat.js'))
+    .pipe(gulp.dest('./public/dist/js'));
+});
+
+gulp.task('compress:css', function() {
+  return gulp.src('./public/src/assets/css/*.css')
+    .pipe(minifyCSS({keepBreaks: true}))
+    .pipe(gulp.dest('./public/dist/css'));
+});
+
+gulp.task('copy:html', function() {
+  return gulp.src('./public/src/**/*.html')
+    .pipe(gulp.dest('./public/dist/'));
+});
+
+gulp.task('copy:fonts', function() {
+  return gulp.src('./public/src/assets/fonts')
+    .pipe(gulp.dest('./public/dist/fonts'));
 });
